ecc4cc8b92c2fda592cb1516794437c0a28254c9
程梦义 <519097965@qq.com>
2022-09-18 13:19:40 +0800

    a

--- app/src/main/java/com/demo/strongbox/fire/Read0914Firebase.kt
+++ app/src/main/java/com/demo/strongbox/fire/Read0914Firebase.kt
@@ -1,16 +1,25 @@
 package com.demo.strongbox.fire
 
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.tencent.mmkv.MMKV
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 object Read0914Firebase {
-
     fun read(){
         saveRaoStr(Local0914Conf.LOCAL_RAO_0914)
         createServerId(Local0914Conf.server0914List)
 
+
+        val remoteConfig = Firebase.remoteConfig
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+            if (it.isSuccessful){
+                saveRaoStr(remoteConfig.getString("strong_rao_0914"))
+            }
+        }
     }
+
 
     private fun saveRaoStr(string: String){
         MMKV.mmkvWithID("strong").encode("strong_json",string)
